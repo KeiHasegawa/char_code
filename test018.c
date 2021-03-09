9794198a063a57b567a17e244b9fe7512898b719
unknown <kei.hasegawa.18@gmail.com>
2021-03-09 17:49:03 +0900

    up memo

---
+++ test018.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("怒");
+  printf("\n");
+  return 0;
+}
+
---
+++ test019.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("倍");
+  printf("\n");
+  return 0;
+}
+
